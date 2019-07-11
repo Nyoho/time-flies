437b233bc75a4d0663c201a6f0770d81bdcab51c
北䑓如法 <algebraicallyClosedField@gmail.com>
2019-07-11 12:32:58 +0900

    Add morgan and compression for expressJS 4.0

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 
 var express = require('express');
+var morgan = require('morgan');
+var compression = require('compression');
 var app = express();
 
 var user = process.env.USER;
@@ -11,10 +13,20 @@ if (user && pass) {
   app.use(express.basicAuth(user, pass));
 }
 
-app.use(express.logger('dev'));
-app.use(express.compress());
+app.use(morgan('dev'));
+app.use(compression({ filter: shouldCompress }));
 app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'), function() {
   console.log('Server listening on port %s', app.get('port'));
 });
+
+function shouldCompress (req, res) {
+  if (req.headers['x-no-compression']) {
+    // don't compress responses with this request header
+    return false
+  }
+
+  // fallback to standard filter function
+  return compression.filter(req, res)
+}
