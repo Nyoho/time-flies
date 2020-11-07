8fd160e0117b243ed444c923ea17702fd85baedc
北䑓如法 <algebraicallyClosedField@gmail.com>
2020-11-07 09:33:27 +0900

    Use requestAnimationFrame
    cf. https://css-tricks.com/using-requestanimationframe-with-react-hooks/

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import ReactDOM from 'react-dom'
 // import Something from './components/something.js'
 // import Lorem from './components/lorem.js'
 
-class Main extends React.Component {
-  render() {
-    this.props.doesNotExist
-
-    return (
-      <div>
-        Hello, {this.props.name}!
-      </div>
-    )
+const Main = () => {
+  const [time, setTime] = useState(0)
+ 
+  const requestRef = useRef();
+  const previousTimeRef = useRef();
+  
+  const animate = time => {
+    if (previousTimeRef.current != undefined) {
+      const deltaTime = time - previousTimeRef.current;
+      
+      setTime(prevTime => (prevTime + deltaTime));
+    }
+    previousTimeRef.current = time;
+    requestRef.current = requestAnimationFrame(animate);
   }
+  
+  useEffect(() => {
+    requestRef.current = requestAnimationFrame(animate);
+    return () => cancelAnimationFrame(requestRef.current);
+  }, []);
+  
+  return <div>{Math.round(time)}</div>
 }
 
-ReactDOM.render(<Main name='Thom'/>, document.getElementById('root'))
+ReactDOM.render(<Main />, document.getElementById('root'))
