8ddd95257a25ecdf621af788ec2cd0b9effab40f
北䑓如法 <algebraicallyClosedField@gmail.com>
2021-05-22 09:08:38 +0900

    Change name (continued fraction)

--- src/components/continued-fraction.js
+++ src/components/continued-fraction.js
@@ -1,6 +1,6 @@
 var Fraction = require('./fraction').Fraction
 
-const continuousFraction = (x, deg) => {
+const continuedFraction = (x, deg) => {
   if (deg == 0) {
     return new Fraction(Math.floor(x), 1)
   }
@@ -9,8 +9,8 @@ const continuousFraction = (x, deg) => {
   if (decPart == 0.0 ) {
     return new Fraction(intPart, 1)
   }
-  const frac = continuousFraction(1 / decPart, deg - 1);
+  const frac = continuedFraction(1 / decPart, deg - 1);
   return new Fraction(intPart * frac.numerator + frac.denominator, frac.numerator)
 }
 
-module.exports.continuousFraction = continuousFraction;
+module.exports.continuedFraction = continuedFraction;
--- src/components/continued-fraction.test.js
+++ src/components/continued-fraction.test.js
@@ -1,8 +1,8 @@
 var Fraction = require('./fraction').Fraction
-var continuousFraction = require('./continuous-fraction').continuousFraction
+var continuedFraction = require('./continued-fraction').continuedFraction
 
 test('0.5 is 1/2', () => {
   const a = new Fraction(1,2);
-  const b = continuousFraction(0.5, 5);
+  const b = continuedFraction(0.5, 5);
   expect(b.toString()).toBe(a.toString())
 })
