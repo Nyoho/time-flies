4d874dba40487865daa57d0442717e75cbd5b527
北䑓如法 <algebraicallyClosedField@gmail.com>
2021-10-05 22:51:26 +0900

    Add a test

--- server.test.js
+++ server.test.js
@@ -7,3 +7,10 @@ test('server is listening to / as an html', done => {
     .expect('Content-Type', /html/)
     .expect(200, done);
 })
+
+test('bundle.js is built and the server returns bundle.js', done => {
+  request(app)
+    .get('/bundle.js')
+    .expect('Content-Type', /javascript/)
+    .expect(200, done);
+})
