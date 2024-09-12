257d1735ec977fe271c0e0253092f1fd02fc8386
北䑓如法 <algebraicallyClosedField@gmail.com>
2024-09-13 07:58:42 +0900

    Delete unneccessary js files

--- public/js/script.js
+++
@@ -1,5 +0,0 @@
-const bar = (a) => {
-  return a+1
-}
-
-module.exports = bar
--- public/js/script.test.js
+++
@@ -1,5 +0,0 @@
-const bar = require('./script')
-
-test('bar is plus one function', () => {
-  expect(bar(3)).toBe(4)
-})
