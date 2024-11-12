3a6e6c34d97c52a43d65905044804730f5704fa0
北䑓如法 <algebraicallyClosedField@gmail.com>
2024-11-12 16:53:01 +0900

    Rewrite ContinuedFraction object to use RationalNumber

--- src/components/continued-fraction.js
+++ src/components/continued-fraction.js
@@ -1,16 +1,16 @@
-import { Fraction } from './fraction'
+import RationalNumber from './rational-number';
 
 const continuedFraction = (x, deg) => {
   if (deg == 0) {
-    return new Fraction(Math.floor(x), 1)
+    return new RationalNumber(Math.floor(x), 1)
   }
   const decPart = x % 1;
   const intPart = Math.floor(x - decPart);
   if (decPart == 0.0) {
-    return new Fraction(intPart, 1)
+    return new RationalNumber(intPart, 1)
   }
-  const frac = continuedFraction(1 / decPart, deg - 1);
-  return new Fraction(intPart * frac.numerator + frac.denominator, frac.numerator)
+  const rat = continuedFraction(1 / decPart, deg - 1);
+  return new RationalNumber(intPart * rat.numerator + rat.denominator, rat.numerator)
 }
 
 export { continuedFraction }
