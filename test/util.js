0f0248ba9fb8b186c5f7c7cd33854a0d2ca4b400
北䑓如法 <algebraicallyClosedField@gmail.com>
2024-11-14 00:02:24 +0900

    Delete unnecessary tests

--- test/util.js
+++
@@ -1,8 +0,0 @@
-// import assert from 'power-assert';
-import { foo } from "../public/js/util";
-
-describe('Foo', function () {
-  it('Foo is something like a function.', function () {
-    assert.equal(foo(10), 20, '2*10 = 20');
-  });
-});
