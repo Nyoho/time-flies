de71f691f260381edf3aa05203e553bbc399a487
北䑓如法 <algebraicallyClosedField@gmail.com>
2024-11-13 22:21:57 +0900

    Change Fraction to RationalNumber

--- src/components/continued-fraction.test.js
+++ src/components/continued-fraction.test.js
@@ -1,8 +1,8 @@
-import { Fraction } from './fraction';
+import RationalNumber from './rational-number';
 import { continuedFraction } from './continued-fraction';
 
 test('0.5 is 1/2', () => {
-  const a = new Fraction(1, 2);
+  const a = new RationalNumber(1, 2);
   const b = continuedFraction(0.5, 5);
   expect(b.toString()).toBe(a.toString())
 })
