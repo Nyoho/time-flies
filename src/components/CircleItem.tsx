f31f99a0ae4cc57beb83e149d7b334690f51a877
北䑓如法 <algebraicallyClosedField@gmail.com>
2025-02-17 21:58:04 +0900

    Add children for CircleItem

--- src/components/CircleItem.tsx
+++ src/components/CircleItem.tsx
@@ -4,13 +4,25 @@ interface CircleItemProps {
   mainText: string
   subText: string
   header: string
+  children?: React.ReactNode
 }
 
-const CircleItem = ({ mainText, subText, header }: CircleItemProps) => {
-  return <>
-    {mainText} {header}
-    {subText}
-  </>
+const CircleItem = ({ mainText, subText, header, children }: CircleItemProps) => {
+
+  return <div className="col-md-2 col-md-offset-1 col-sm-12 text-center">
+    <div className="service-item">
+      <div className="circle-box-container">
+        <div className="circle-box">
+          <div className="cell" onClick={() => ''}>
+            {mainText}<br />
+            {subText}
+          </div>
+        </div>
+      </div>
+      <h4>{header}</h4>
+      {children}
+    </div>
+  </div>
 }
 
 export default CircleItem
