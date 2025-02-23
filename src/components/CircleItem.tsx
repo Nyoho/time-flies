382a375bc61e5f7c95fd6b2024369dbffb69c7ae
北䑓如法 <algebraicallyClosedField@gmail.com>
2025-02-23 20:16:43 +0900

    Apply CircleItem to the 'remain' area and fix style

--- src/components/CircleItem.tsx
+++ src/components/CircleItem.tsx
@@ -9,13 +9,12 @@ interface CircleItemProps {
 
 const CircleItem = ({ mainText, subText, header, children }: CircleItemProps) => {
 
-  return <div className="col-md-2 col-md-offset-1 col-sm-12 text-center">
+  return <div className="col-md-2 col-sm-6 text-center">
     <div className="service-item">
       <div className="circle-box-container">
         <div className="circle-box">
           <div className="cell" onClick={() => ''}>
-            {mainText}<br />
-            {subText}
+            <div><span className="main-part">{mainText}</span> {subText}</div>
           </div>
         </div>
       </div>
