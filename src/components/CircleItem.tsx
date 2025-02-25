a160d61269d5a2d2b21c91239bd846e36e438a2c
北䑓如法 <algebraicallyClosedField@gmail.com>
2025-02-26 00:57:36 +0900

    Improve CircleItem component

--- src/components/CircleItem.tsx
+++ src/components/CircleItem.tsx
@@ -1,25 +1,72 @@
 import React from 'react'
+import { CSSTransition } from 'react-transition-group'
 
 interface CircleItemProps {
   mainText: string
   subText: string
   header: string
   children?: React.ReactNode
+  flippable?: boolean
+  flipped?: boolean
+  onFlip?: () => void
+  frontContent?: React.ReactNode
+  backContent?: React.ReactNode
 }
 
-const CircleItem = ({ mainText, subText, header, children }: CircleItemProps) => {
+const CircleItem = ({
+  mainText,
+  subText,
+  header,
+  children,
+  flippable = false,
+  flipped = false,
+  onFlip,
+  frontContent,
+  backContent,
+}: CircleItemProps) => {
+  const defaultContent = (
+    <div>
+      <span className="main-part">{mainText}</span> {subText}
+    </div>
+  )
+
   return (
     <div className="col-md-2 col-sm-6 text-center">
       <div className="service-item">
-        <div className="circle-box-container">
-          <div className="circle-box">
-            <div className="cell" onClick={() => ''}>
-              <div>
-                <span className="main-part">{mainText}</span> {subText}
+        {flippable ? (
+          <div
+            className="animate-switch-container"
+            onClick={() => {
+              if (onFlip) onFlip()
+            }}
+          >
+            <div className="circle-box-container">
+              <CSSTransition in={!flipped} timeout={500} classNames="flip" unmountOnExit>
+                <div className="circle-box">
+                  <div className="cell">
+                    {frontContent || defaultContent}
+                  </div>
+                </div>
+              </CSSTransition>
+
+              <CSSTransition in={flipped} timeout={500} classNames="flip" unmountOnExit>
+                <div className="circle-box">
+                  <div className="cell">
+                    {backContent}
+                  </div>
+                </div>
+              </CSSTransition>
+            </div>
+          </div>
+        ) : (
+          <div className="circle-box-container">
+            <div className="circle-box">
+              <div className="cell">
+                {defaultContent}
               </div>
             </div>
           </div>
-        </div>
+        )}
         <h4>{header}</h4>
         {children}
       </div>
