1f9fdd9b0f2aa6a1030e5ea234563b4bae48ac94
北䑓如法 <algebraicallyClosedField@gmail.com>
2025-05-18 20:31:10 +0900

    Update setting of size-limit

--- .size-limit.ts
+++ .size-limit.ts
@@ -2,6 +2,13 @@ import type { SizeLimitConfig } from '../../packages/size-limit'
 
 module.exports = [
   {
+    name: "JavaScript bundle",
     path: "dist/assets/*.js",
+    ignore: [],
+  },
+  {
+    name: "CSS bundle",
+    path: "dist/assets/*.css",
+    ignore: [],
   }
 ] satisfies SizeLimitConfig
